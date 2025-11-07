ab4a8cc3fd76eb3a5a0bc764651418d1254a853a
GreekyFTW <109968697+GreekyFTW@users.noreply.github.com>
2025-11-07 17:10:48 +0200

    fucking idk the code

--- src/Client_TCP.java
+++ src/Client_TCP.java
@@ -43,18 +43,19 @@ public class Client_TCP {
         //StringBetweenMarkers(bytes,list.get(1).toString().to)
 
         //String Hexadecimal = HexFormat.of().formatHex(bytes);
-        String Hexadecimal = Arrays.toString(bytes).
+        String String1 = Arrays.toString(bytes).
                 replaceAll(",","").
                 replaceAll(" ","").
                 replace("[","").
                 replace("]","");
 
         System.out.println(Arrays.toString(bytes));
-        System.out.println(Hexadecimal);
+        System.out.println(String1);
 
         ////This fucking thing has binary mixed with hex (i lied)
-        //We replace 14(space) with 00100000(space)
-        String FixedString = Hexadecimal.
+        ////We replace 14(space) with 00100000(space) ( we actually dont anymore)
+        //Terrible replacing of values because why not
+        String FixedString = String1.
                 replaceAll("99","00100000").
                 replaceAll("2","S").
                 replaceAll("3","E");
@@ -62,20 +63,25 @@ public class Client_TCP {
 
         System.out.println("Fixed String: " + FixedString);
 
+        // Removing markers
         String string =  StringBetweenMarkers(FixedString, StartFlag, EndFlag).
                 replaceAll("S","").
                 replaceAll("E","");;
 
         System.out.println("String: "+string);
 
+        //Just says the word penis for testing
         //String test = "0111000001100101011011100110100101110011";
 
+        ////Finally decoding the binary
         System.out.println(Arrays
                 .stream(string.split("(?<=\\G.{8})"))
                 .map(s -> Character.toString((char)Integer.parseInt(s, 2)))
                 .collect(Collectors.joining())
         );
 
+
+        //Close the socket why not
         socket.close();
 
     }
@@ -88,69 +94,30 @@ public class Client_TCP {
         List list = new ArrayList();
 
         while(i <bytes.length){
-            if(bytes[i]==StartFlag){ iStart=i; System.out.println("Transmition Start Byte : "+ iStart); list.add(i); }
-            if(bytes[i]==EndFlag){ iEnd=i;  System.out.println("Transmition End Byte : "+ iEnd); list.add(i);}
+            if(bytes[i]==StartFlag){ iStart=i; System.out.println("Transmission Start Byte : "+ iStart); list.add(i); }
+            if(bytes[i]==EndFlag){ iEnd=i;  System.out.println("Transmission End Byte : "+ iEnd); list.add(i);}
 
             //Take care of spaces
             if(bytes[i]== 20) bytes[i]=99;
 
             i++;
-
         }
         return list;
     }
 
     //flags are useless for now
     public static String StringBetweenMarkers (String string , char flag1, char flag2) {
-//        int i=0;
-//
-//        int iByte=0;
-//
-//        byte[] bytes1 = new byte[4096];
-//        int stringSize = 0;
-//
-//        while(i<list.size()-1) {
-//            if ((list.get(i + 1) - list.get(i)) == 1) {
-//            }else{
-//                stringSize = list.get(i + 1) - list.get(i);
-//
-//                while (iByte<stringSize && iByte<4096){
-//                    bytes1[iByte] = bytes[i];
-//                    iByte= iByte + 1;
-//                    //System.out.println(iByte-1);
-//                }
-//            }
-//            i++;
-//        }
-//
-//        System.out.println(stringSize);
-//
-//
-//        String Hexadecimal = HexFormat.of().formatHex(bytes1);
-//
-//        System.out.println(Hexadecimal);
-//
-//        String FixedString = Hexadecimal.
-//                replaceAll("14","00100000").
-//                replaceAll("3","0010000000100000").
-//                replaceAll("2","0010000000100000");
-//
-//        return FixedString;
-
-        String result = "failed";
+        String string1 = "01000110 01100001 01101001 01101100 01100101 01100100 00100000 01110100 01101111 00100000 01100111 01100101 01110100 00100000 01110000 01101111 01101001 01101110 01110100 01110011 ";
 
         int startIndex = string.indexOf('S');
-
-        // to find the index of the end character after the start character
         int endIndex = string.indexOf('E', startIndex + 1);
 
-        // check if both start and end characters are found
         if (startIndex != -1 && endIndex != -1) {
-            // extract the substring between the start and end characters
-             result = string.substring(startIndex + 1, endIndex);
+
+            string1 = string.substring(startIndex + 1, endIndex);
 
         }
-        return result;
+        return string1;
     }
 
 }
